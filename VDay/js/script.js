fbd3935779dda80ee48cb102ace6ee73530b4d4b
Carlosl93 <Carlosl93>
2017-02-14 17:48:47 -0430

    Fix Letter

--- VDay/js/script.js
+++ VDay/js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
         $('.fa').attr('class', 'fa fa-heart fa-4x');
       } else if(clicks > 30){
         $('.cont').fadeOut(function(){
-          $('.container').html('<div class="letter"> <p>Otro año mas en el que te tomo de la mano.</p> <p>Otro año mas en el que la relacion evoluciona, va mutando y formandose a medida que ambos cambiamos, va creciendo y haciendose mas solida a medida que el tiempo pasa.</p> <p>Pero es alli en el centro de este caos, en el nucleo de nuestro amor, que se encuentran dos personas agarradas de la mano fuertemente.</p> <i class="fa fa-heart fa-1x" aria-hidden="true"></i> <p>!Te amo!</p> <p> ¡Gracias por ser una vez mas mi valentin! </p></div>').fadeIn();
+          $('.container').html('<div class="letter"> <p>Otro año mas en el que te tomo de la mano.</p> <p>Otro año mas en que la relacion, como nosotros, muta y evoluciona.</p> <p>Hemos aprendido a complementarnos y a medida que el tiempo pasa, la relacion se hace -y nos hace- mas fuertes.</p><p>Todo ese universo de amor, con el caos que lo acompaña, que hemos creado juntos, se concentra en su nucleo, donde estamos tu y yo, tomados de la mano y lo vemos pasar mientras pasamos por el.</p> <i class="fa fa-heart fa-1x" aria-hidden="true"></i> <p>!Te amo!</p> <p> ¡Gracias por ser una vez mas mi valentin! </p></div>').fadeIn();
         });
 
       }
