37eb4ea9236baa1fcc3fc2fab8b266e37568ccec
Carlosl93 <Carlosl93>
2017-06-08 11:36:47 -0430

    IOS fix

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ $(document).ready(() => {
   // Click on projectBox
   $('.projectBox').css('cursor','pointer');
   $(document).on('click', '.projectBox', (e) => {
+
+    e.preventDefault();
+
     $(e.target).toggleClass('projectBoxActive');
 
     if($(e.target).attr('data-isshow') == 'true'){
